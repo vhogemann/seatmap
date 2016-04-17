e139b76e72a1b928f5f75fc42cbe992ad35706d6
Victor Hogemann <victor.hogemann@ingresso.com>
2016-04-17 14:38:09 -0300

    Views

--- src/seatmap.ts
+++ src/seatmap.ts
@@ -10,12 +10,13 @@ namespace SeatMap {
         /** should we try to enable WebGL, if available */
         enable_web_gl : boolean;
         /** view implementation for drawing seats */
-        seat_view_class : View.ISeatView;
+        seat_view_class : View.ASeatView;
         /** view implementation for drawing the map */
         map_view_class : View.IMapView;
     }
     
     export class Map {
+        
         private seats_map : {[key:string]:Model.Seat};
         
         constructor( el: HTMLElement, data: any, options:IMapOptions ){
@@ -46,13 +47,25 @@ namespace SeatMap {
             container: PIXI.Container;
         }
         
-        export interface ISeatView extends View {
+        export interface IViewConfig{
+            
+        }
+        
+        export abstract class ASeatView implements View {
             /** internal state of the view */
             seat : Model.Seat;
-            getBase() : PIXI.Sprite;
-            getText() : PIXI.Sprite;
-            getIcon() : PIXI.Sprite;
-            addEventListener(listener:IEventListener<ISeatView>);
+            config : IViewConfig;
+            container : PIXI.Container;
+            
+            constructor(seat:Model.Seat, config: IViewConfig){
+                this.seat = seat;
+                this.config = config;
+            }
+            
+            abstract getBase() : PIXI.Sprite;
+            abstract getText() : PIXI.Sprite;
+            abstract getIcon() : PIXI.Sprite;
+            abstract addEventListener(listener:IEventListener<ASeatView>);
         }
         
         export interface IEventListener<T extends View>  {
---
+++ src/view.ts
@@ -0,0 +1,11 @@
+/// <reference path="./seatmap.ts" />
+
+namespace SeatMap{
+    export namespace View {
+        
+        export class DefaultSeatView extends ASeatView{
+            
+        }
+        
+    }
+}
